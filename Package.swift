be46ffb6f9a504b503e2d81449f1bc93ced10ea6
Mo Ramezanpoor <me@mohsenr.com>
2025-08-30 10:38:16 +0100

    Create a package that can monitor the process’s logs and persist them for later use.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LogMonitor",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17),
+        .tvOS(.v17),
+        .watchOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "LogMonitor",
+            targets: ["LogMonitor"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/zuhlke/Support.git", from: "4.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "LogMonitor"
+        ),
+    ]
+)
